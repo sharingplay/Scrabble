9136e0c345a8b29104dc682fb37abf85c14dfd40
edgargch17 <42707360+edgargch17@users.noreply.github.com>
2019-03-25 01:50:25 -0600

    Socket_Cliente

---
+++ Socket_Cliente/cmake-build-debug/CMakeFiles/Socket_Cliente.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Socket_Cliente.dir/main.cpp.o"
+  "Socket_Cliente.pdb"
+  "Socket_Cliente"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Socket_Cliente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
