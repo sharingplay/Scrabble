d995c211cca3d681e33f7271addbf4ba458471cd
edgargch17 <42707360+edgargch17@users.noreply.github.com>
2019-03-29 13:36:41 -0600

    Socket Cliente con la libreria nlohmann

--- Socket_Cliente/main.cpp
+++ Socket_Cliente/main.cpp
@@ -4,8 +4,14 @@
 #include <cstdlib>
 #include <cstring>
 #include <stdlib.h>
+#include <iostream>
+#include "nlohmann/json.hpp"
+
+using namespace std;
 
 int main(void) {
+
+    using json = nlohmann::json;
 
     struct sockaddr_in direccionServidor;
     direccionServidor.sin_family = AF_INET;
@@ -18,13 +24,49 @@ int main(void) {
         return 1;
     }
 
-    while (1){
-        char mensaje[1000];
-        scanf("%s", mensaje);
+    //json json_cliente
+    auto jsonCliente = R"(
+            {
+                "Nombre": "Edgar",
+                "Deporte": "Futbol",
+                "Edad": 19
+            }
+        )"_json;
 
+    //para mostrar el JSON
+    //cout << jsonCliente << endl;
 
-        send(cliente, mensaje, strlen(mensaje), 0);
+    //para mostrar si JSON es is_objet()
+    //cout << jsonCliente.is_object() << endl;
+
+    //Para enviar los valores de las claves
+    //nc 127.0.0.1 8080;
+
+    //claves con sus valores
+    for (auto& el : jsonCliente.items()){
+        //scanf("%s", element);
+        //send(cliente, el.value(),, 0);
+        cout << el.key() << " : " << el.value() << '\n';
     }
 
+    /*//Para enviarle el json
+    json* mensaje[jsonCliente.size()];
+    mensaje = *&jsonCliente;
+    send(cliente, mensaje , sizeof(mensaje), 0);
+    cout << mensaje << '\n';
+    //para mantenerlo abierto
+    while (1){
+
+    }*/
+
+    send(cliente, "HOLLFokkT", 10, 0);
+    json* buff = &jsonCliente;
+    send(cliente, buff, sizeof(buff),0);
+
+    /*//Para enviar mensajes type char con nc 127.0.0.1 8080
+    while (1){
+        char mensaje[WCHAR_MAX];
+        send(cliente, mensaje , sizeof(mensaje), 0);
+    }*/
     return 0;
 }
